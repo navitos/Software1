1e5cc2806b231b13446e33f3d26f6dea2dd68708
navitos <2220211015@estudiantesunibague.edu.co>
2024-08-23 21:22:15 -0500

    add

---
+++ script.js
@@ -0,0 +1,9 @@
+// Agrega funcionalidad de scroll suave para el menú de navegación
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
